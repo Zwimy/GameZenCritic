d2b143996b304506874aa57899f5678329f745b2
Yordan <yordan.koparanov@gmail.com>
2024-02-25 10:41:55 +0200

    Add project files.

---
+++ GameZenCritic.Infrastructure/Data/GameZenDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameZenCritic.Infrastructure.Data
+{
+    public class GameZenDb : IdentityDbContext
+    {
+        public GameZenDb(DbContextOptions<GameZenDb> options)
+            : base(options)
+        {
+        }
+    }
+}
