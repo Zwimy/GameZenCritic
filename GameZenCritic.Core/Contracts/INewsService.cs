63cd4c8c31c68b4fc213eefddba41ac10bffac18
Yordan <yordan.koparanov@gmail.com>
2024-03-17 10:26:06 +0200

    Added requirements for INewsService

--- GameZenCritic.Core/Contracts/INewsService.cs
+++ GameZenCritic.Core/Contracts/INewsService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GameZenCritic.Core.Models.Developer;
+using GameZenCritic.Core.Models.News;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,18 @@ namespace GameZenCritic.Core.Contracts
 {
     public interface INewsService
     {
+        Task<AllNewsQueryViewModel> GetAllAsync(
+            string? searchTerm = null,
+            int currentPage = 1
+            );
+
+        Task<AllNewsQueryViewModel> GetByGameIdAsync(
+            int gameId,
+            string? searchTerm = null,
+            int currentPage = 1
+            );
+
+        Task<NewsDetailViewModel> GetByNewsIdAsync(int id);
 
     }
 }
---
+++ GameZenCritic.Core/Services/NewsService.cs
@@ -0,0 +1,36 @@
+﻿using GameZenCritic.Core.Contracts;
+using GameZenCritic.Core.Models.News;
+using GameZenCritic.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameZenCritic.Core.Services
+{
+    public class NewsService : INewsService
+    {
+        private readonly IRepository repository;
+
+        public NewsService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public Task<AllNewsQueryViewModel> GetAllAsync(string? searchTerm = null, int currentPage = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AllNewsQueryViewModel> GetByGameIdAsync(int gameId, string? searchTerm = null, int currentPage = 1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NewsDetailViewModel> GetByNewsIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
